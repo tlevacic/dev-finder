e853359146b59aa766e321b602134e4c3e203f23
Tin August Levacic <tin.levacic@verybigthings.com>
2024-03-28 10:55:17 +0100

    chore: drizzle init

---
+++ db/schema.ts
@@ -0,0 +1,9 @@
+import {
+    pgTable,
+    text
+} from "drizzle-orm/pg-core";
+
+export const testing = pgTable("testing", {
+    id: text("id").notNull().primaryKey(),
+    name: text("name"),
+  });
